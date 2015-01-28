615d22b23c1c2181d2d8788ee1f79e0c4245d67a
Schamper <mrwafflewaffle@aim.com>
2015-01-28 15:56:26 +0100

    Small optimizations, new ! prefix for ultra-lite linking

--- library.js
+++ library.js
@@ -2,27 +2,33 @@
 	"use strict";
 
 	var YoutubeLite = {},
-		embed = '<div class="js-lazyYT" data-youtube-id="$1" data-width="640" data-height="360"><iframe class="lazytube" src="//www.youtube.com/embed/$1"></iframe></div>';
+		embed = '<div class="js-lazyYT" data-youtube-id="$2" data-width="640" data-height="360">Loading...</div>',
+		link = '<div class="youtube-lite"><i class="fa fa-film youtube-lite-play"></i> <a class="youtube-lite-link" href="https://youtube.com/watch?=v=$2">$3</a>' + embed + '</div>',
 
-	var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
-	var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
-	var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
+		expressions = [
+			/(!?)<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">(.+)<\/a>/g,
+			/(!?)<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">(.+)<\/a>/g,
+			/(!?)<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">(.+)<\/a>/
+		];
 
 	YoutubeLite.parse = function(data, callback) {
 		if (!data || !data.postData || !data.postData.content) {
 			return callback(null, data);
 		}
-		if (data.postData.content.match(embedUrl)) {
-			data.postData.content = data.postData.content.replace(embedUrl, embed);
-		}
-		if (data.postData.content.match(regularUrl)) {
-			data.postData.content = data.postData.content.replace(regularUrl, embed);
-		}
-		if (data.postData.content.match(shortUrl)) {
-			data.postData.content = data.postData.content.replace(shortUrl, embed);
-		}
-		callback(null, data);
 
+		var matches, html, content = data.postData.content;
+
+		for (var i = 0, l = expressions.length; i < l; i++) {
+			matches = expressions[i].exec(content);
+			if (matches) {
+				html = matches[1].length ? link : embed;
+				content = content.replace(expressions[i], html);
+			}
+		}
+
+		data.postData.content = content;
+
+		callback(null, data);
 	};
 
 	module.exports = YoutubeLite;
--- static/lib/main.js
+++ static/lib/main.js
@@ -4,7 +4,19 @@ $(window).on('action:widgets.loaded', function() {
 	$('.js-lazyYT').lazyYT();
 });
 
-
 $(window).on('action:posts.loaded', function(){
 	$('.js-lazyYT').delay(500).lazyYT();
 });
+
+$(document).ready(function() {
+	$(document).off('click.youtube-lite', '.youtube-lite-play').on('click.youtube-lite', '.youtube-lite-play', function(e) {
+		e.preventDefault();
+
+		var el = $(e.currentTarget).parents('.youtube-lite').find('.js-lazyYT');
+		if (el.css('display') !== 'none') el.hide();
+		else el.show();
+
+		return false;
+	});
+});
+
